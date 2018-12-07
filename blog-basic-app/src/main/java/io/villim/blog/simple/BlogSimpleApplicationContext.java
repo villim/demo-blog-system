46129d9fa38e97261e6442c89b9367241f8e4edf
William Wang <william.wang@AspiraConnect.com>
2018-12-07 22:41:56 +0800

    Add Basic statndard moudle for researching

---
+++ blog-basic-app/src/main/java/io/villim/blog/simple/BlogSimpleApplicationContext.java
@@ -0,0 +1,8 @@
+package io.villim.blog.simple;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"io.github.villim.blog.domain"})
+public class BlogSimpleApplicationContext {
+
+}
